b0644ea63424dcf5d3334fde3cf3bdfec49c0ccf
Sadat Ishraq <sadatishraq@gmail.com>
2026-09-08 22:39:58 +0600

    Fix profile stylesheet loading

--- views/customer/profile.php
+++ views/customer/profile.php
@@ -22,7 +22,7 @@ $edit = $_GET["edit"] ?? "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile - CineVerse</title>
 
-    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/profile.css?v=2">
 </head>
 
 <body>
--- views/customer/updateProfile.php
+++ views/customer/updateProfile.php
@@ -20,7 +20,7 @@ $customer = getCustomerById($customerId);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Profile - CineVerse</title>
-    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="stylesheet" href="../css/profile.css?v=2">
 </head>
 
 <body>
