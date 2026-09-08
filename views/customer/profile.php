30eaad22b93c5e639473b3e4c24057eafcd3e21c
Sadat Ishraq <sadatishraq@gmail.com>
2026-09-08 06:24:34 +0600

    Reorganize project structure

--- views/customer/profile.php
+++ views/customer/profile.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineVerse - User Profile</title>
-    <link rel="stylesheet" href="profile_style.css">
+    <link rel="stylesheet" href="../css/profile.css">
 </head>
 
 <body>
